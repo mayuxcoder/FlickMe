6b401d104248c8773edaa6b2dcfae39f214c5afe
mayuran kunarajah <oopmayu@gmail.com>
2017-01-22 08:14:43 +0800

    Initial Commit

---
+++ FlickedMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickedMe
+//
+//  Created by Mayu on 22/1/17.
+//  Copyright © 2017 Mayu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
